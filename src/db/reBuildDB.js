e7a2a546671b2dee418b1f611a74ecabbbf86665
nadav1996700 <nadav1996700@gmail.com>
2022-06-15 10:16:42 +0300

    finish initialize db from json file and lands with random price values

--- src/db/reBuildDB.js
+++ src/db/reBuildDB.js
@@ -8,23 +8,36 @@ function reset() {
   Land.find({}).deleteMany().exec();
   User.find({}).deleteMany().exec();
 
-  let jsonPath = path.join(__dirname, "..", "static", "usersData.json");
-  let data = fs.readFileSync(jsonPath, "utf8");
+  const jsonPath = path.join(__dirname, "..", "static", "usersData.json");
+  const data = fs.readFileSync(jsonPath, "utf8");
   const users_data = JSON.parse(data);
   // populate the users collection from json data
   for (var i = 0; i < users_data.length; i++) {
     new User(users_data[i]).save();
   }
 
-  jsonPath = path.join(__dirname, "..", "static", "landsData.json");
-  data = fs.readFileSync(jsonPath, "utf8");
-  const landsData = JSON.parse(data);
-  // populate the lands collection from json data
-  for (var i = 0; i < landsData.length; i++) {
-    new Land(landsData[i]).save();
+  // populate the lands collectio
+  const numberOfLands = 100;
+  const parkLands = [50, 60, 70, 80];
+  const roadLands = [52, 53, 54, 55, 56, 57, 62, 72];
+  const max = 200,
+    min = 15;
+  for (var i = 0; i < numberOfLands; i++) {
+    let isPark = parkLands.some((element) => element === i);
+    let isRoad = roadLands.some((element) => element === i);
+    let canBeSale = !isPark && !isRoad && i < 40 ? true : false;
+    new Land({
+      id: i,
+      owner: "Nadav.Ltd",
+      can_be_sale: canBeSale,
+      price: canBeSale ? Math.floor(Math.random() * (max - min + 1) + min) : 0,
+      game: "",
+      isPark: isPark,
+      isRoad: isRoad,
+    }).save();
   }
 
-  console.log("data saved on mongo db");
+  console.log("initialized data on mongo db");
 }
 
 module.exports = { reset };
