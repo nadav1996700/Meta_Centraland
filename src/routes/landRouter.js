855e07413a48513600b20853cfdac96872d28439
nadav1996700 <nadav1996700@gmail.com>
2022-06-15 19:35:13 +0300

    add getAllLands request

--- src/routes/landRouter.js
+++ src/routes/landRouter.js
@@ -1,21 +1,18 @@
-// LandRouter.js
-
 const express = require("express");
 const LandRouter = express.Router();
-
 const Land = require("../db/Land");
 
-// add new Land
-LandRouter.route("/create").post(function (req, res) {
-  const land = new Land(req.body);
-  land
-    .save()
-    .then((land) => {
-      res.json("Land added successfully");
-    })
-    .catch((err) => {
-      res.status(400).send("unable to save to database");
-    });
+// get all Lands
+LandRouter.route("/getAllLands").get(function (req, res) {
+  Land.find({}, function (err, lands) {
+    if (err) {
+      console.log("Error: " + err);
+      res.status(400).send("error occured!");
+      return;
+    } else {
+      res.send(lands);
+    }
+  });
 });
 
 module.exports = LandRouter;
